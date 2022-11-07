de853698bf00bd17fa71fcaadf8900ef370598dd
FrcM <38992176+Franck5568@users.noreply.github.com>
2022-11-07 23:22:49 +0100

    gestion fichier de sauvegarde

--- packages/player_info.py
+++ packages/player_info.py
@@ -1,10 +1,20 @@
+import os
+from pathlib import Path
+
 class playerInfo:
- 
-    def __init__(self, page: int, rang:int, bg_tag:str, quote:int) -> None:
-        self.rank = rang
-        self.account = bg_tag
-        self.rating = quote
-        self.page = page
+    
+    JSON_FILENAME ='.halloffame.json'
 
     def get_player_info(self) -> dict:
         return {'rank':self.rank,'rating':self.rating,'page':self.page}
+
+    @classmethod
+    def load_json(cls) -> dict:
+        cls.savefile = os.path.join(Path.home(),cls.JSON_FILENAME)
+        if os.path.exists(cls.savefile):
+            print('exist')
+        else:
+            return {}
+
+if __name__ == '__main__':
+    playerInfo.load_json()
